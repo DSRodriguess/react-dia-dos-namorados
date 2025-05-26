675f0fc862003496153ed0780591199ee8f53c6d
David da Silva Rodrigues <rodrigues.david@graduao.uerj.br>
2025-05-26 17:08:50 -0300

    Inclusão da mensagem final com musica

--- src/App.jsx
+++ src/App.jsx
@@ -238,9 +238,31 @@ function App() {
         <>
           <h1>💝 Surpresa! 💝</h1>
           <p>Essas são apenas algumas das nossas memórias juntos...<br />Te amo muito! 💖</p>
+
           <ImageSlider />
+
+          <div className="mensagem-final">
+            <h2>Amor</h2>
+            <p>
+              Desde o dia em que te conheci, minha vida se encheu de cor, sorrisos e momentos inesquecíveis. <br />
+              Cada foto aqui é uma lembrança do quanto você é especial pra mim. <br />
+              Obrigado por ser quem você é e por estar comigo. <br />
+              Te amo infinitamente! 💘
+            </p>
+          </div>
+
+          {/* Música de fundo do YouTube */}
+          <iframe
+            width="0"
+            height="0"
+            src="https://www.youtube.com/embed/lp-EO5I60KA?autoplay=1&loop=1&playlist=lp-EO5I60KA"
+            title="Música romântica"
+            frameBorder="0"
+            allow="autoplay"
+          />
         </>
       )}
+
 
     </div>
   );
